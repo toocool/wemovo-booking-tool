c68accb724441ecabc88673644b59e8a488ff62a
toocool <draodakum@gmail.com>
2016-02-24 09:44:19 +0100

    added authorization token and changed the url to the gds

--- public/api/destinations.php
+++ public/api/destinations.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://book.eurolines.ch/api/destinations/".$_GET['id'];
+$url="http://gds.wemovo.com/api/destinations/".$_GET['id'];
 //  Initiate curl
 $ch = curl_init();
 // Disable SSL verification
@@ -8,6 +8,9 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // Set the url
 curl_setopt($ch, CURLOPT_URL,$url);
+$headers = array();
+$headers[] = 'Authorization: Token 31cd43a444fec801c44299abd62cab7871e37922';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 // Execute
 $result=curl_exec($ch);
 // Closing
--- public/api/origin.php
+++ public/api/origin.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://book.eurolines.ch/api/origins/?q=".$_GET['q'];
+$url="http://gds.wemovo.com/api/origins/?q=".$_GET['q'];
 //  Initiate curl
 $ch = curl_init();
 // Disable SSL verification
@@ -8,6 +8,10 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // Set the url
 curl_setopt($ch, CURLOPT_URL,$url);
+
+$headers = array();
+$headers[] = 'Authorization: Token 31cd43a444fec801c44299abd62cab7871e37922';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 // Execute
 $result=curl_exec($ch);
 // Closing
